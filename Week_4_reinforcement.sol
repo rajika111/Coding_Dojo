200e82b68e5bce668983bbcbf62bbec40b3ff3fa
raji alharthi <rajika11@hotmail.com>
2021-02-23 02:32:14 +0300

    Solved reunforcement

--- Week_4_reinforcement.sol
+++ Week_4_reinforcement.sol
@@ -15,17 +15,36 @@
 pragma solidity >=0.6.0 <0.8.0;
 
 contract Math{
+    
+    
 
     function isMultiple(uint8 _n, uint8 _m) public pure returns(bool) {
         
-        // Write your code here
-    
+        // Write your code 
+        uint8 _remainder = 0;
+        uint8 zero = 0;
+        bool _isBool = false;
+        uint8 n = _n;
+        uint8 m = _m;
+        
+                
+        _remainder =  n %  m ;
+                
+        if (_remainder == zero){
+                    
+            _isBool = true;
+                    
+        }
+                
+        return _isBool;
+
     }
+    
     
     function max(uint256 _n, uint256 _m) public pure returns(uint256) {
-        // Write your code here   
-    
+        return _n ^ ((_n ^ _m) & -(_n << _m));
     
     }
+    
 
 }

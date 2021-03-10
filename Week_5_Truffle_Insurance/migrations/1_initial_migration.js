dbc4930acaecbf6fba2e3f0f767022c9ead6d82c
raji alharthi <rajika11@hotmail.com>
2021-03-10 15:46:40 +0300

    Truffle_Test

--- Week_5_Truffle_Insurance/migrations/1_initial_migration.js
+++ Week_5_Truffle_Insurance/migrations/1_initial_migration.js
@@ -1,5 +1,5 @@
-const Migrations = artifacts.require("Migrations");
+const TecInsurance = artifacts.require("TecInsurance");
 
 module.exports = function (deployer) {
-  deployer.deploy(Migrations);
+  deployer.deploy(TecInsurance);
 };
